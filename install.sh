56711a83fc07e47ec6d37759e5de608252737304
Daniel Hernandez <danielhp95@gmail.com>
2017-10-11 12:14:34 +0100

    Added google chrome installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function head_install_chrome() {
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 	
+    sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
+} 
+
+
+function head_install_vlc() {
+    sudo apt-get install vlc
+}
